4e549b943853257ba6c5c49b0b13f3038c8ac46b
Phil D <pcdrummond@icloud.com>
2023-11-11 19:32:57 -0800

    Updates breaches module.

--- src/breaches/breaches.module.ts
+++ src/breaches/breaches.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BreachesService } from './breaches.service';
 import { BreachesController } from './breaches.controller';
+import { Breach, BreachSchema } from '../schemas/breaches.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Breach.name, schema: BreachSchema }]),
+  ],
+  controllers: [BreachesController],
   providers: [BreachesService],
-  controllers: [BreachesController]
 })
 export class BreachesModule {}

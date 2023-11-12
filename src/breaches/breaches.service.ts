50d03a66dfaba37cbb12509815d63eb452b19cb6
Phil D <pcdrummond@icloud.com>
2023-11-11 19:31:19 -0800

    Adds dot.env.

--- src/breaches/breaches.service.ts
+++ src/breaches/breaches.service.ts
@@ -3,6 +3,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateBreachDto } from './dto/create-breach.dto';
 import { Breach } from './entities/breach.entity';
+import * as fs from 'fs';
+import * as csv from 'csv-parser';
+import { BreachCsvDto } from './dto/breach-csv.dto';
 
 @Injectable()
 export class BreachesService {
@@ -29,5 +32,21 @@ export class BreachesService {
 
   async remove(id: string): Promise<any> {
     return this.breachModel.findByIdAndRemove(id).exec();
+  }
+
+  async parseCsvFile(filePath: string): Promise<BreachCsvDto[]> {
+    const results: BreachCsvDto[] = [];
+
+    return new Promise((resolve, reject) => {
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+          resolve(results);
+        })
+        .on('error', (error) => {
+          reject(error);
+        });
+    });
   }
 }

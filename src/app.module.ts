23a326087cd2e186a2b1c140a58fb86e85816311
Phil D <pcdrummond@icloud.com>
2023-11-11 19:34:12 -0800

    Updates main and app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BreachesModule } from './breaches/breaches.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, BreachesModule],
+  imports: [
+    AuthModule,
+    BreachesModule,
+    MongooseModule.forRoot(''),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })

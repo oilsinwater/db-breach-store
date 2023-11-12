d807d233e89071c029cb4d25dfe9ca27160d00f6
Phil D <pcdrummond@icloud.com>
2023-11-11 18:20:53 -0800

    Adds auth to app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BreachesModule } from './breaches/breaches.module';
 
 @Module({
-  imports: [BreachesModule],
+  imports: [AuthModule, BreachesModule],
   controllers: [AppController],
   providers: [AppService],
 })
